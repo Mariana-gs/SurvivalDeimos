f62834977e049277b399ed49bfa5db06593bd23a
Mariana Soares <81964220+Mariana-gs@users.noreply.github.com>
2023-06-15 13:58:56 -0300

    Personagem Principal Criado
    Personagem Criado e Inputs Adicionados

---
+++ Source/SurvivalDeimos/Private/PersonagemTPS.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PersonagemTPS.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+APersonagemTPS::APersonagemTPS(){
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmCamera = CreateDefaultSubobject<USpringArmComponent>(FName("SpringArmCamera"));
+	SpringArmCamera->TargetArmLength = 200.f;
+	SpringArmCamera->bUsePawnControlRotation = true;
+	SpringArmCamera->AddRelativeLocation(FVector(0.f, 40.f, 50.f));
+	SpringArmCamera->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(SpringArmCamera);
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+}
+
+// Called when the game starts or when spawned
+void APersonagemTPS::BeginPlay(){
+	Super::BeginPlay();
+
+}
+
+void APersonagemTPS::MoveForward(float Value){
+	AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void APersonagemTPS::MoveRight(float Value){
+	AddMovementInput(GetActorRightVector() * Value);
+}
+
+// Called every frame
+void APersonagemTPS::Tick(float DeltaTime){
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APersonagemTPS::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent){
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &APersonagemTPS::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APersonagemTPS::MoveRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &APersonagemTPS::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APersonagemTPS::AddControllerYawInput);
+
+}
+
---
+++ Source/SurvivalDeimos/Public/PersonagemTPS.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PersonagemTPS.generated.h"
+
+UCLASS()
+class SURVIVALDEIMOS_API APersonagemTPS : public ACharacter{
+	
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APersonagemTPS();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		class UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+		class USpringArmComponent* SpringArmCamera;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

5f3b18b53d52ba5af7c1de95632c68d2c85c4887
Mariana Soares <81964220+Mariana-gs@users.noreply.github.com>
2023-06-16 15:02:35 -0300

    Inimigo Adicionado
    Adicionand Inimigo com Movimentos

---
+++ Source/SurvivalDeimos/Private/BotAIController.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BotAIController.h"
+#include "Perception/PawnSensingComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Pawn.h"
+#include "BotCharacter.h"
+#include "Engine/Engine.h"
+#include "Arma.h"
+
+
+
+ABotAIController::ABotAIController(){
+	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(FName("PawnSensingComp"));
+	PawnSensingComp->SensingInterval = 0.25f;
+	PawnSensingComp->bOnlySensePlayers = true;
+	PawnSensingComp->SetPeripheralVisionAngle(180.f);
+	PawnSensingComp->SightRadius = 3000.f;
+
+	BehaviorTreeComp = CreateDefaultSubobject<UBehaviorTreeComponent>(FName("BehaviorTreeComp"));
+	BlackBoardComp = CreateDefaultSubobject<UBlackboardComponent>(FName("BlackBoardComp"));
+}
+
+void ABotAIController::OnPossess(APawn* InPawn){
+	Super::OnPossess(InPawn);
+	PawnSensingComp->OnSeePawn.AddDynamic(this, &ABotAIController::OnSeePawn);
+	if (BehaviorTree) {
+		BlackBoardComp->InitializeBlackboard(*BehaviorTree->BlackboardAsset);
+		BehaviorTreeComp->StartTree(*BehaviorTree);
+		BlackBoardComp->SetValueAsBool("DeveAndar", true);
+	}
+}
+
+void ABotAIController::OnSeePawn(APawn* SensedPawn){
+	if (BlackBoardComp && SensedPawn) {
+		GEngine->AddOnScreenDebugMessage(-1,3.f, FColor::Red, TEXT("Estou te Vendo"));
+		BlackBoardComp->SetValueAsObject("Inimigo", SensedPawn);
+		BlackBoardComp->SetValueAsBool("DeveAndar", false);
+
+		BlackBoardComp->SetValueAsBool("DeveAndar", false);
+		ABotCharacter* Bot = Cast <ABotCharacter>(GetPawn());
+		Bot->ArmaInimigo->Atirar();
+
+	}
+}
---
+++ Source/SurvivalDeimos/Private/BotCharacter.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BotCharacter.h"
+#include "Engine/World.h"
+#include "Arma.h"
+#include "Engine/EngineTypes.h"
+
+
+// Sets default values
+ABotCharacter::ABotCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABotCharacter::BeginPlay(){
+	Super::BeginPlay();
+
+	//Spawn Weapon
+	FActorSpawnParameters Params;
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	ArmaInimigo = GetWorld()->SpawnActor<AArma>(BP_Rifle, FTransform(), Params);
+
+	ArmaInimigo->AttachToComponent(Cast<USceneComponent>(GetMesh()), 
+		FAttachmentTransformRules::SnapToTargetIncludingScale, FName("WeaponSocket"));
+
+
+	
+}
+
+// Called every frame
+void ABotCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABotCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
